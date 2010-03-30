209857785e287a0af265b10cfa10a610ba2bfee4
Greg Symons <gsymons@gsconsulting.biz>
2010-03-29 22:30:15 -0400

    No, really, the test passes now.
    Ok, so I'm still gitting used to git. Somewhere I missed the part (probably when
    I didn't actually RTFM at all) where I had to explicitly tell it what to include
    in the commit. Cool feature, wish I'd known about it before I committed a
    broken build.
    
    This commit has the code and resources necessary for the layout committed on the
    last commit.

--- bptrack/src/biz/gsconsulting/bptrack/activities/AddMeasurement.java
+++ bptrack/src/biz/gsconsulting/bptrack/activities/AddMeasurement.java
@@ -1,7 +1,14 @@
 package biz.gsconsulting.bptrack.activities;
 
+import biz.gsconsulting.bptrack.R;
 import android.app.Activity;
+import android.os.Bundle;
 
 public class AddMeasurement extends Activity {
-
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		setContentView(R.layout.add_measurement);
+	}
 }
